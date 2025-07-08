23c0bd45714933f5e0ad4e467c4a7817172f5c6a
Pathumi Ranasinghe <162596287+PathumiRanasinghe@users.noreply.github.com>
2025-07-08 14:27:03 +0530

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/home_screen.dart';
 // import 'screens/signup_screen.dart';
 // import 'screens/login_screen.dart';
 
-void main() {
+Future<void> main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const TravelonApp());
 }
 
